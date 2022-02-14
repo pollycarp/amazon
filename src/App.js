b2a3025272d4741864cf4e5162d3224889befcb7
kingslayer254 <markpollycarp@gmail.com>
2022-02-15 00:56:28 +0300

    00:56

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
     <h1> fff</h1>
-    {/*Header*/}
+    <Header/>
     {/*Home*/}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,45 @@
 import React from 'react'
-import './Header.css '
+//import './Header.css '
 
 function Header() {
   return (
-    <div className='header'>
+  <div className='header'>
       <img
-       className ="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+       clas sName ="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
     />
 
     <div 
     className="header__search">
-      
+      <input
+      className="header__searchInput" type="text"
+      />
+      {/*logo*/}
+
     </div>
+
+    <div className="header__nav">
+      <div className='header__option'>
+          <span 
+          className='header__optionLineone'>Hello Guest</span>
+          <span
+          className='header__optionLineTwo'>Sign In</span>
+      </div>
+
+      <div className='header__option'>
+          <span 
+          className='header__optionLineone'>Returns</span>
+          <span
+          className='header__optionLineTwo'>&Order</span>
+      </div>
+
+      <div className='header__option'>
+          <span 
+          className='header__optionLineone'>Your</span>
+          <span
+          className='header__optionLineTwo'>Prime</span>
+      </div>
     </div>
+  </div>
   )
 }
 
