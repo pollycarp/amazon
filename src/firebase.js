e94cf4073fd5b9d9ebb61021d1173b304e6d7f4f
kingslayer254 <markpollycarp@gmail.com>
2022-02-14 10:30:57 +0300

    master

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCb3-t50o1kEXzMRds4jQzg49srvUeMzVg",
+    authDomain: "kingslayer-clone.firebaseapp.com",
+    projectId: "kingslayer-clone",
+    storageBucket: "kingslayer-clone.appspot.com",
+    messagingSenderId: "979550719455",
+    appId: "1:979550719455:web:800a0ce0edc2348970886a",
+    measurementId: "G-3D7ZF027Z4"
+  };
