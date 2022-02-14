6749f51336775bf4e9df1e170c8db6a377ddb5b4
kingslayer254 <markpollycarp@gmail.com>
2022-02-14 12:11:56 +0300

    14th 12.11

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+    <h1> fff</h1>
+    {/*Header*/}
+    {/*Home*/}
     </div>
   );
 }
