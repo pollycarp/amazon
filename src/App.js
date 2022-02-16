f11f5507958232397d80346250f9f0ed38180c45
kingslayer254 <markpollycarp@gmail.com>
2022-02-16 20:00:22 +0300

    20:00

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
-
+import Home from './Home'
 
 function App() {
   return (
     //BEM
     <div className="app">
     <Header/>
-    {/*Home*/}
+    <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return <div className="Home">
+      <div className="home__container">
+        <img src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
+        alt="imgerror"
+        />
+        <div className="home__raw">
+            {}
+        </div>
+      </div>
+  </div>;
+  
+}
+
+export default Home
