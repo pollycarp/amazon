08fee94262172f4687dbf9dc0d7e38f65ad573f5
kingslayer254 <markpollycarp@gmail.com>
2022-02-17 12:04:19 +0300

    12:04

--- src/Product.js
+++ src/Product.js
@@ -2,8 +2,18 @@ import React from 'react'
 import "./Product.css"
 
 function Product() {
-  return <div className="product">
-      
+  return <div 
+  className="product">
+     <div className='product__info'>
+         <p>The lean startup</p>
+         <p className='product__price'>
+             <small>$</small>
+             <strong>20.2</strong>
+         </p>
+         <div className='product__rating'>
+            <p>🌟</p> 
+         </div>
+     </div> 
     </div>
   
 }
